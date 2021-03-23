cebf37882b07da49eefbc3269e3ba1c9350f6d07
Jabberwockyz <20187131@itla.edu.do>
2021-03-22 23:29:34 -0400

    Examen Parcial 02 - Ejercicio 02

---
+++ Examen Parcial 02 - Ejercicio 02.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Examen_Parcial_02___Ejercicio_02
+{
+    class Program
+    {
+        static string CargarNombre()
+        {
+            Console.WriteLine("Introduzca un nombre:");
+            string Nombre = Console.ReadLine();
+            Console.Clear();
+            return Nombre;
+        }
+        static int CargarSalario()
+        {
+            Console.WriteLine("Introduzca el salario:");
+            int Salario = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            return Salario;
+        }
+        static void SalarioMayor()
+        {
+            string[] Nombres = new string[5];
+            int[] Sueldos = new int[5];
+            int C;
+            for (C = 0; C <= 4; C++)
+            {
+                Nombres[C] = CargarNombre();
+                Sueldos[C] = CargarSalario();
+            }
+            int Mayor = Sueldos.Max();
+            int i;
+            int N = 0;
+            for (i = 0; i <= 4; i++)
+            {
+                if (Sueldos[i] == Mayor)
+                {
+                    N = i;
+                }
+            }
+            Console.WriteLine("El empleado con el mayor salario es {0}", Nombres[N]);
+            Console.WriteLine("Tiene un salario de: {0}", Mayor);
+        }
+        static void Main(string[] args)
+        {
+            SalarioMayor();
+        }
+    }
+}
